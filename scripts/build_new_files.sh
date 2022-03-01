fc46d7b9570a1e2cd73cb515064b00a4a122068a
Daniel Selsam <dselsam@openai.com>
2022-03-01 15:34:49 -0800

    fix script

--- scripts/build_new_files.sh
+++ scripts/build_new_files.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 git diff --name-only origin/main | while read FILE ; do
-    echo "Try ${FILE}"
     if [ -f ${FILE} ];
     then
-        echo "Found ${FILE}"
-        if [[ ${FILE} == "*.lean" ]];
+        NAME=$(basename ${FILE})
+        if [[ ${NAME} =~ (.*)?\.lean ]];
         then
-            echo "Running ${FILE}"
-            lean ${FILE} | python3 scripts/detect_errors.py
+            lean --json -T100000 ${FILE} | python3 scripts/detect_errors.py
         fi
     fi
 done    
