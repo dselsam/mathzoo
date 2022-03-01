fe194f49873f6695ae2c2299b8306c2256dba241
Daniel Selsam <dselsam@openai.com>
2022-03-01 08:58:01 -0800

    Revert "chore: use 'and' instead of ',' in author list"
    This reverts commit 820839b3507b67856b760a585ea6f58236001654.

--- src/mathzoo/olympiads/imo/2020/p2.lean
+++ src/mathzoo/olympiads/imo/2020/p2.lean
@@ -2,7 +2,7 @@
 /-
 Copyright (c) 2020 Joseph Myers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Myers and Yury Kudryashov
+Authors: Joseph Myers, Yury Kudryashov
 -/
 
 import analysis.mean_inequalities
